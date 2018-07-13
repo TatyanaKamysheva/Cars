35048bb90c91c6bb455c0dfa200f609d9687998e
kamyshevat <tak_nn@mail.ru>
2018-07-13 14:57:16 +0300

    Add list

--- src/main/java/com/cars/server/dao/DAO.java
+++ src/main/java/com/cars/server/dao/DAO.java
@@ -1,24 +1,20 @@
 package com.cars.server.dao;
 
-import com.cars.shared.models.Manager;
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.metadata.ClassMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Map;
 
 @Repository
-public class ManagerDAO<T> {
+public class DAO<T> {
 
     @Autowired
     private SessionFactory sessionFactory;
 
-    private static final Logger logger = Logger.getLogger(ManagerDAO.class);
+    private static final Logger logger = Logger.getLogger(DAO.class);
 
     @Autowired
     public void setSessionFactory(SessionFactory sessionFactory) {
@@ -33,8 +29,6 @@ public class ManagerDAO<T> {
     @SuppressWarnings("unchecked")
     public List<T> list(String className) {
         Session session = this.sessionFactory.getCurrentSession();
-        //List<T> list = session.createQuery("from Manager").list();
-        logger.info(className);
         List<T> list = session.createQuery("from "+className).list();
         return list;
     }
--- src/main/java/com/cars/server/service/ManagerService.java
+++ src/main/java/com/cars/server/service/ManagerService.java
@@ -1,6 +1,6 @@
 package com.cars.server.service;
 
-import com.cars.server.dao.ManagerDAO;
+import com.cars.server.dao.DAO;
 import com.cars.shared.models.Manager;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,23 +12,23 @@ import java.util.List;
 @Service
 public class ManagerService {
     @Autowired
-    private ManagerDAO<Manager> managerDAO;
+    private DAO<Manager> DAO;
 
-    public void setManagerDAO(ManagerDAO managerDAO) {
-        this.managerDAO = managerDAO;
+    public void setDAO(DAO DAO) {
+        this.DAO = DAO;
     }
 
     Logger logger = Logger.getLogger(ManagerService.class);
 
     @Transactional
     public void save(Manager manager) {
-        this.managerDAO.save(manager);
+        this.DAO.save(manager);
     }
 
     @Transactional
     public List<Manager> listManagers() {
 
-        return this.managerDAO.list(Manager.class.getSimpleName());
+        return this.DAO.list(Manager.class.getSimpleName());
     }
 
 }
